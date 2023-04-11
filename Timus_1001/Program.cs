f27a9bc24c3f95d1e0da1441fa2c5f91e7633f4c
Marcin Gruszczyk <m.k.gruszczyk@gmail.com>
2023-04-11 23:08:44 +0200

    Add project files.

---
+++ Timus_1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var inputs = System.Text.RegularExpressions.Regex.Split(Console.In.ReadToEnd(), @"\s+").ToList();
+        inputs.RemoveAll(i => !long.TryParse(i, out _));
+
+        inputs.Reverse();
+        foreach (var i in inputs)
+        {
+            Console.WriteLine((decimal)Math.Sqrt(long.Parse(i)));
+        }
+    }
+}
